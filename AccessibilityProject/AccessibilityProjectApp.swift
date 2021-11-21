cc14cc3204373f9b7a28c1273802a7adeca14f6e
Yash Poojary <yashspoojary245@gmail.com>
2021-11-21 17:00:47 +0530

    Initial Commit

---
+++ AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Yash Poojary on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
