d7f6a8582769ce7075c7725c14096a1d08f18fc9
Yash Poojary <yashspoojary245@gmail.com>
2021-11-21 19:04:10 +0530

    Entire project

--- AccessibilityProject/ContentView.swift
+++ AccessibilityProject/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rating = 5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Stepper("Rate our services: \(rating)/5", value: $rating, in: 1...5)
+            .accessibilityValue(Text("\(rating)"))
     }
 }
 
@@ -19,3 +22,49 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Your score is")
+//            Text("1000")
+//        }
+//        .accessibilityElement(children: .ignore)
+//        .accessibilityLabel(Text("Your score is"))
+//
+//
+//    }
+//}
+
+// struct ContentView: View {
+//
+//    let pictures = [
+//        "ales-krivec-15949",
+//        "galina-n-189483@2x",
+//        "kevin-horstmann-141705",
+//        "nicolas-tissot-335096"
+//    ]
+//
+//    let labels = [
+//        "Tulips",
+//        "Mangoes",
+//        "Sunflowers",
+//        "Fireworks"
+//    ]
+//
+//    @State private var selectedImage = Int.random(in: 0...3)
+//
+//
+//    var body: some View {
+//        Image(pictures[selectedImage])
+//            .resizable()
+//            .scaledToFit()
+//            .accessibilityLabel(labels[selectedImage])
+//            .accessibilityAddTraits(.isButton)
+//            .accessibilityAddTraits(.isImage)
+//            .onTapGesture {
+//                selectedImage = Int.random(in: 0...3)
+//            }
+//
+//    }
+//}
